886a97a14b84a904143332b7ded0830cf555d7a2
Lavi sharma <lavipandit123@gmail.com>
2024-11-27 04:45:25 +0530

    signup and signin done

---
+++ api/User.js
@@ -0,0 +1,150 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('./../models/User');
+
+// password handler
+const bcrypt = require('bcrypt');
+
+//signup
+router.post('/signup', (req, res) => {
+    let {name, email, password, dateOfBirth} = req.body;
+    name = name.trim();
+    email = email.trim();
+    password = password.trim();
+    dateOfBirth = dateOfBirth.trim();
+
+    if (name == "" || email == "" || password == "" || dateOfBirth == "") {
+        res.json({
+            status: "FAILED",
+            message: "Empty input fields!"
+        });
+    } else if (!/^[a-zA-Z]+(\s[a-zA-Z]+)*$/.test(name)) {
+        res.json({
+            status: "FAILED",
+            message: "Invalid name entered"
+        })
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+        res.json({
+            status: "Failed",
+            message: "invalied email entered"
+        })
+    } else if (!new Date(dateOfBirth).getTime()) {
+        res.json({
+            status: "FAILED",
+            message: "Invalied date of birth entered"
+        })
+    } else if (password.length < 8) {
+        res.json({
+            status: "FAILED",
+            message: "password is too short!"
+        })
+    } else {
+        //checking is user already exists
+        User.find({email}).then(result => {
+            if (result.length) {
+                //already exists
+                res.json({
+                    status: "FAILED",
+                    message: "User  with the provided email already exists"
+                })
+            } else {
+                //try to create new user
+
+            //password handling
+            const saltRounds = 10;
+            bcrypt.hash(password, saltRounds).then(hashedPassword => {
+                const newUser = new User({
+                    name,
+                    email,
+                    password: hashedPassword,
+                    dateOfBirth
+                });
+
+                newUser.save().then(result => {
+                    res.json({
+                        status: "SUCCESS",
+                        message: "Signup successful",
+                        data: result,
+                    })
+                })
+                .catch(err => {
+                    res.json({
+                        status: "FAILED",
+                        message: "An error occured while saving user account!"
+                        })
+                })
+            })
+            .catch(err => {
+                res.json({
+                status: "FAILED",
+                message: "An error occured while hashing passowrd!"
+                })
+            })
+            }
+        }).catch(err => {
+            console.log(err);
+            res.json({
+                status: "FAILED",
+                message: "An error occured while checking for existing user!"
+            })
+        })
+    }
+})
+
+//signin
+router.post('/signin', (req, res) => {
+    let {email, password} = req.body;
+    email = email.trim();  
+    password = password.trim();
+
+    if (email == "" || password == "") {
+        res.json({
+            status: "FAILED",
+            message: "Empty credentials supplied"
+        })
+    } else {
+        //check if user exist
+        User.find({email}).then(data=> {
+            if (data.length) {
+                //user exists
+
+                const hashedPassword = data[0].password;
+                bcrypt.compare(password, hashedPassword).then(result => {
+                    if (result) {
+                        //password match
+                        res.json({
+                            status: "SUCCESS",
+                            message: "Signin successful",
+                            data: data
+                        })
+                    } else {
+                        res.json({
+                            status: "FAILED",
+                            message: "Invalid password entered!"
+                        })
+                    }
+                })
+                .catch(err => {
+                    res.json({
+                        status: "FAILED",
+                        message: "An error occured while comparing passwords"
+                    })
+                })
+            } else {
+                res.json({
+                    status: "FAILED",
+                    message: "Invalid credentials entered!"
+                })
+            }
+        })
+        .catch(err => {
+            res.json({
+                status: "FAILED",
+                message: "An error occured while checking for existing user"
+            })
+        })
+    }
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+require('./config/db');
+
+const app = require('express')();
+const port = 3001;
+
+const UserRouter = require('./api/User');
+
+const bodyParser = require('express').json;
+app.use(bodyParser());
+
+app.use('/user', UserRouter)
+
+app.listen(port, () => {
+    console.log('server is running on port ${port}');
+})
