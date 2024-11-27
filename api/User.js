b6e1be3f0fa203fee6b4a85a75f9e4e104d70cf8
Lavi sharma <lavipandit123@gmail.com>
2024-11-27 16:32:23 +0530

    added the bcryptjs file

--- api/User.js
+++ api/User.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const User = require('./../models/User');
 
 // password handler
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 //signup
 router.post('/signup', (req, res) => {
